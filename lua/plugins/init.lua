2a06c551af1830d69e522f60590e3971ae599325
hyzm <990473625@qq.com>
2024-04-17 21:37:32 +0800

    add 'expand selection' config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,95 +1,136 @@
-return {{
+return {
+  {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     config = function()
-        require "configs.conform"
-    end
-}, -- These are some examples, uncomment them if you want to see them work!
-{
+      require "configs.conform"
+    end,
+  }, -- These are some examples, uncomment them if you want to see them work!
+  {
     "neovim/nvim-lspconfig",
     config = function()
-        local lsp_zero = require('lsp-zero')
-        lsp_zero.extend_lspconfig()
-        require("nvchad.configs.lspconfig").defaults()
-        require "configs.lspconfig"
-    end
-}, {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x'
-}, {
+      local lsp_zero = require "lsp-zero"
+      lsp_zero.extend_lspconfig()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
-        ensure_installed = {"lua-language-server", "stylua", "html-lsp", "css-lsp", "prettier", "rust-analyzer",
-                            "typescript-language-server", "tailwindcss-language-server"}
-    }
-}, {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "rust-analyzer",
+        "typescript-language-server",
+        "tailwindcss-language-server",
+      },
+    },
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-        ensure_installed = {"vim", "lua", "vimdoc", "html", "css", "rust", "typescript"}
-    }
-}, {
-    'mrcjkb/rustaceanvim',
-    version = '^4',
-    ft = {'rust'}
-}, {
-    'mfussenegger/nvim-dap',
-    config = function()
-        local dap, dapui = require("dap"), require("dapui")
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-    end
-}, {
-    "folke/neodev.nvim",
-    opts = {}
-}, {
-    "rcarriga/nvim-dap-ui",
-    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
-    config = function()
-        require("dapui").setup();
-        require("neodev").setup({
-            library = {
-                plugins = {"nvim-dap-ui"},
-                types = true
-            }
-        })
-    end
-}, {
-    'akinsho/flutter-tools.nvim',
-    lazy = false,
-    dependencies = {'nvim-lua/plenary.nvim', 'stevearc/dressing.nvim' -- optional for vim.ui.select
+      ensure_installed = { "vim", "lua", "vimdoc", "html", "css", "rust", "typescript" },
     },
     config = function()
-        require("flutter-tools").setup {
-            debugger = {
-                enabled = true,
-                run_via_dap = true,
-                exception_breakpoints = {},
-                register_configurations = function(paths)
-                    require("dap").configurations.dart =
-                        { -- For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
-                        {
-                            name = "Debug Android",
-                            request = "launch",
-                            type = "dart",
-                            args = {"--flavor", "android", "--target", "lib/main_dev.dart"}
-                        }, {
-                            name = "Debug iOS",
-                            request = "launch",
-                            type = "dart",
-                            args = {"--target", "lib/main_dev.dart"}
-                        }}
-                end
-            }
-        }
-    end
-}}
+      require("nvim-treesitter.configs").setup {
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            node_incremental = "v",
+            node_decremental = "V",
+          },
+        },
+      }
+    end,
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    ft = { "rust" },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap, dapui = require "dap", require "dapui"
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "folke/neodev.nvim",
+    opts = {},
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      require("dapui").setup()
+      require("neodev").setup {
+        library = {
+          plugins = { "nvim-dap-ui" },
+          types = true,
+        },
+      }
+    end,
+  },
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = function()
+      require("flutter-tools").setup {
+        debugger = {
+          enabled = true,
+          run_via_dap = true,
+          exception_breakpoints = {},
+          register_configurations = function(paths)
+            require("dap").configurations.dart =
+              { -- For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+                {
+                  name = "Debug Android",
+                  request = "launch",
+                  type = "dart",
+                  args = { "--flavor", "android", "--target", "lib/main_dev.dart" },
+                },
+                {
+                  name = "Debug iOS",
+                  request = "launch",
+                  type = "dart",
+                  args = { "--target", "lib/main_dev.dart" },
+                },
+              }
+          end,
+        },
+      }
+    end,
+  },
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    lazy = false,
+    --   config = function()
+    --     -- require('illuminate').configure({});
+    --   end,
+  },
+}
